3ac4e798145ae9533cc4c074cdaf1f634a7ea930
Carlos <carlosthe19916@gmail.com>
2015-04-24 17:17:09 -0500

    sgMaxDate

--- src/sg-utils.js
+++ src/sg-utils.js
@@ -56,9 +56,18 @@
             return result[1];
         }
     });
-
-
-
+    
+    moduleSgUtils.directive('sgMaxDate', function() {
+        return {
+            require: 'ngModel',
+            link: function($scope, elem, attrs, ngModel) {
+                ngModel.$validators.sgmaxdate = function(modelValue,viewValue){
+                    var value = modelValue || viewValue;
+                    return $scope.maxDate >= value;
+                }
+            }
+        };
+    });
 
 
 
