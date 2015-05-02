65a72cedd3307a03d614bec19a37378853ff0b06
carlos <carlosthe19916@gmail.com>
2015-05-02 13:54:36 -0500

    add SGDialog

--- src/sg-utils.js
+++ src/sg-utils.js
@@ -72,6 +72,110 @@
 
 
 
+    moduleSgUtils.service('SGDialog', function($modal) {
+        var dialog = {};
+
+        var openDialog = function(title, message, btns) {
+            var controller = function($scope, $modalInstance, title, message, btns) {
+                $scope.title = title;
+                $scope.message = message;
+                $scope.btns = btns;
+
+                $scope.ok = function () {
+                    $modalInstance.close();
+                };
+                $scope.cancel = function () {
+                    $modalInstance.dismiss('cancel');
+                };
+            };
+
+            return $modal.open({
+                template: ''+
+                "<div class=\"modal-header\">\n" +
+                "<button type=\"button\" class=\"close\" ng-click=\"cancel()\">\n" +
+                "<span class=\"pficon pficon-close\"></span>\n" +
+                "</button>\n" +
+                "<h4 class=\"modal-title\">{{title}}</h4>\n" +
+                "</div>\n" +
+                "<div class=\"modal-body\">{{message}}</div>\n" +
+                "<div class=\"modal-footer\">\n" +
+                "<button type=\"button\" data-ng-class=\"btns.cancel.cssClass\" ng-click=\"cancel()\">{{btns.cancel.label}}</button>\n" +
+                "<button type=\"button\" data-ng-class=\"btns.ok.cssClass\" ng-click=\"ok()\">{{btns.ok.label}}</button>\n" +
+                "</div>\n" +
+                "",
+                controller: controller,
+                resolve: {
+                    title: function() {
+                        return title;
+                    },
+                    message: function() {
+                        return message;
+                    },
+                    btns: function() {
+                        return btns;
+                    }
+                }
+            }).result;
+        };
+
+        var escapeHtml = function(str) {
+            var div = document.createElement('div');
+            div.appendChild(document.createTextNode(str));
+            return div.innerHTML;
+        };
+
+        dialog.confirmDelete = function(name, type, success) {
+            var title = 'Eliminar ' + escapeHtml(type.charAt(0).toUpperCase() + type.slice(1));
+            var msg = '¿Estas seguro de querer eliminar permanentemente el/la ' + type + ' ' + name + '?';
+            var btns = {
+                ok: {
+                    label: 'Eliminar',
+                    cssClass: 'btn btn-danger'
+                },
+                cancel: {
+                    label: 'Cancelar',
+                    cssClass: 'btn btn-default'
+                }
+            };
+
+            openDialog(title, msg, btns).then(success);
+        };
+
+        dialog.confirmGenerateKeys = function(name, type, success) {
+            var title = 'Generate new keys for realm';
+            var msg = 'Are you sure you want to permanently generate new keys for ' + name + '?';
+            var btns = {
+                ok: {
+                    label: 'Generate Keys',
+                    cssClass: 'btn btn-danger'
+                },
+                cancel: {
+                    label: 'Cancel',
+                    cssClass: 'btn btn-default'
+                }
+            };
+
+            openDialog(title, msg, btns).then(success);
+        };
+
+        dialog.confirm = function(title, message, success, cancel) {
+            var btns = {
+                ok: {
+                    label: title,
+                    cssClass: 'btn btn-danger'
+                },
+                cancel: {
+                    label: 'Cancel',
+                    cssClass: 'btn btn-default'
+                }
+            };
+
+            openDialog(title, message, btns).then(success, cancel);
+        };
+
+        return dialog
+    });
+
 
     /**
      * Module sg-utils-iso3166.
@@ -118,7 +222,7 @@
      * Estas clases dependen de sg-ubigeo.
      */
 
-    moduleSgUtilsUbigeo.directive('sgUbigeo',function(SGUbigeo){
+    moduleSgUtilsUbigeo.directive('sgUbigeo',['SGUbigeo', function(SGUbigeo){
         return {
             restrict:'E',
             replace: false,
@@ -271,7 +375,7 @@
             +'</div>'
             +'</div>'
         }
-    });
+    }]);
 
 
 
@@ -286,6 +390,28 @@
      * Estas clases dependen de sg-persona.
      */
 
+
+
+
+
+
+
+    angular.module("sgtemplate/modal/modal.html", []).run(["$templateCache", function($templateCache) {
+        $templateCache.put("sgtemplate/modal/modal.html",
+            "<div class=\"modal-header\">\n" +
+            "<button type=\"button\" class=\"close\" ng-click=\"cancel()\">\n" +
+            "<span class=\"pficon pficon-close\">×</span>\n" +
+            "</button>\n" +
+            "<h4 class=\"modal-title\">{{title}}</h4>\n" +
+            "</div>\n" +
+            "<div class=\"modal-body\">{{message}}</div>\n" +
+            "<div class=\"modal-footer\">\n" +
+            "<button type=\"button\" data-ng-class=\"btns.cancel.cssClass\" ng-click=\"cancel()\">{{btns.cancel.label}}</button>\n" +
+            "<button type=\"button\" data-ng-class=\"btns.ok.cssClass\" ng-click=\"ok()\">{{btns.ok.label}}</button>\n" +
+            "</div>\n" +
+            ""
+        );
+    }]);
 
 
 
