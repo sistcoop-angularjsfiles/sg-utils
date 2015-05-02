35ae33dd53caaad35d7a9bfa5e108e22a8c69a5a
carlos <carlosthe19916@gmail.com>
2015-05-02 13:12:15 -0500

    sgUtils update to SGUbigeo

--- src/sg-utils.js
+++ src/sg-utils.js
@@ -118,7 +118,7 @@
      * Estas clases dependen de sg-ubigeo.
      */
 
-    moduleSgUtilsUbigeo.directive('sgUbigeo',function(SGDepartamento, SGProvincia, SGDistrito){
+    moduleSgUtilsUbigeo.directive('sgUbigeo',function(SGUbigeo){
         return {
             restrict:'E',
             replace: false,
@@ -135,7 +135,7 @@
                         return (value.length == 6  || value.length == 0);
                 };
 
-                SGDepartamento.$search().then(function(data){
+                SGUbigeo.$search().then(function(data){
                     $scope.departamentos = data;
                     $scope.activeListener();
                 });
@@ -151,7 +151,7 @@
                 $scope.$watch('ubigeo.departamento', function(){
                     if(!angular.isUndefined($scope.ubigeo.departamento) && $scope.ubigeo.departamento){
                         $scope.provincias = $scope.ubigeo.departamento.$getProvincias().$object;
-                        ngModel[1].$setDirty();
+                        ngModel[0].$setDirty();
                     } else {
                         $scope.ubigeo.provincia = undefined;
                         $scope.ubigeo.distrito = undefined;
@@ -162,7 +162,7 @@
                 });
                 $scope.$watch('ubigeo.provincia', function(){
                     if(!angular.isUndefined($scope.ubigeo.provincia) && $scope.ubigeo.provincia){
-                        $scope.distritos = SGProvincia.$getDistritos($scope.ubigeo.departamento.codigo, $scope.ubigeo.provincia.codigo).$object;
+                        $scope.distritos = SGUbigeo.$getDistritos($scope.ubigeo.departamento.ubigeoDepartamento, $scope.ubigeo.provincia.ubigeoProvincia).$object;
                     } else {
                         $scope.ubigeo.distrito = undefined;
 
@@ -171,7 +171,7 @@
                 });
                 $scope.$watch('ubigeo.distrito', function(){
                     if(!angular.isUndefined($scope.ubigeo.distrito) && $scope.ubigeo.distrito){
-                        var ubigeo = $scope.ubigeo.departamento.codigo + $scope.ubigeo.provincia.codigo + $scope.ubigeo.distrito.codigo;
+                        var ubigeo = $scope.ubigeo.distrito.ubigeo;
                         ngModel[1].$setViewValue(ubigeo);
                     }
                 });
@@ -185,7 +185,7 @@
                             && angular.isUndefined($scope.distritos)){
 
                             for(var i=0;i<$scope.departamentos.length;i++){
-                                if($scope.departamentos[i].codigo == ngModel[1].$modelValue.substring(0, 2)){
+                                if($scope.departamentos[i].ubigeoDepartamento == ngModel[1].$modelValue.substring(0, 2)){
                                     $scope.ubigeo.departamento = $scope.departamentos[i];
                                     break;
                                 }
@@ -194,17 +194,17 @@
                             $scope.ubigeo.departamento.$getProvincias().then(function(data){
                                 $scope.provincias = data;
                                 for(var i=0;i<$scope.provincias.length;i++){
-                                    if($scope.provincias[i].codigo == ngModel[1].$modelValue.substring(2, 4)){
+                                    if($scope.provincias[i].ubigeoProvincia == ngModel[1].$modelValue.substring(2, 4)){
                                         $scope.ubigeo.provincia = $scope.provincias[i];
                                         break;
                                     }
                                 }
 
-                                SGProvincia.distritos($scope.ubigeo.departamento.codigo, $scope.ubigeo.provincia.codigo).then(function(data){
+                                SGUbigeo.$getDistritos($scope.ubigeo.departamento.ubigeoDepartamento, $scope.ubigeo.provincia.ubigeoProvincia).then(function(data){
                                     $scope.distritos = data;
 
                                     for(var i=0;i<$scope.distritos.length;i++){
-                                        if($scope.distritos[i].codigo == ngModel[1].$modelValue.substring(4, 6)){
+                                        if($scope.distritos[i].ubigeoDistrito == ngModel[1].$modelValue.substring(4, 6)){
                                             $scope.ubigeo.distrito = $scope.distritos[i];
                                             break;
                                         }
